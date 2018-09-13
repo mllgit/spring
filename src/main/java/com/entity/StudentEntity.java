2325b922af5a1052845b368285e8a3381107d4e9
mllgit <1248656005@qq.com>
2018-09-13 10:55:07 +0800

    spring 整合hibernate

---
+++ src/main/java/com/entity/StudentEntity.java
@@ -0,0 +1,66 @@
+package com.entity;
+
+import javax.persistence.*;
+
+@Entity//标识这是一个实体类
+//表名和数据库名一样可以省略
+@Table(name = "student")
+public class StudentEntity {
+    private int id;
+    private String name;
+    private int age;
+//主键
+    @Id
+    //如果两个字段名字一样，可以不写//nullable 不为空
+    @Column(name = "id", nullable = false)//标识字id段与数据库字段间的映射关系
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "name", nullable = false, length = 16)
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Basic
+    @Column(name = "age", nullable = false)
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        StudentEntity that = (StudentEntity) o;
+
+        if (id != that.id) return false;
+        if (age != that.age) return false;
+        if (name != null ? !name.equals(that.name) : that.name != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + age;
+        return result;
+    }
+}
---
+++ src/test/java/com/entity/StudentEntityTest.java
@@ -0,0 +1,26 @@
+package com.entity;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.orm.hibernate5.HibernateTemplate;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.junit.Assert.*;
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations ={"classpath:spring.xml"} )
+public class StudentEntityTest {
+    @Autowired
+    HibernateTemplate hibernateTemplate;
+
+    @Test
+    public void testGet(){
+        StudentEntity studentEntity = hibernateTemplate.get(StudentEntity.class, 1);
+        studentEntity.setName("tom");
+        hibernateTemplate.update(studentEntity);
+        //System.out.println(studentEntity);
+
+    }
+}
